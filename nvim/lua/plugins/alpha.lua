9cf7f3817f6a96a99abfef85d07eecb33e1e7cd5
MAOZ <maooz4426@gmail.com>
2026-03-16 08:52:48 +0900

    alphaを軽くセットアップ

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -5,6 +5,39 @@ return {
         'nvim-lua/plenary.nvim'
     },
     config = function()
-        require 'alpha'.setup(require 'alpha.themes.theta'.config)
+        local alpha = require 'alpha'
+        local dashboard = require 'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+            [[   .-') _   ('-.                    (`-.            _   .-')      ]],
+            [[  ( OO ) )_(  OO)                 _(OO  )_        ( '.( OO )_     ]],
+            [[,--./ ,--,'(,------. .-'),-----. ,--(_/   ,. \ ,-.-') ,--.   ,--.)]],
+            [[|   \ |  |\ |  .---'( OO'  .-.  '\   \   /(__/ |  |OO)|   `.'   | ]],
+            [[|    \|  | )|  |    /   |  | |  | \   \ /   /  |  |  \|         | ]],
+            [[|  .     |/(|  '--. \_) |  |\|  |  \   '   /,  |  |(_/|  |'.'|  | ]],
+            [[|  |\    |  |  .--'   \ |  | |  |   \     /__),|  |_.'|  |   |  | ]],
+            [[|  | \   |  |  `---.   `'  '-'  '    \   /   (_|  |   |  |   |  | ]],
+            [[`--'  `--'  `------'     `-----'      `-'      `--'   `--'   `--' ]],
+        }
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+        }
+        local handle = io.popen('fortune')
+        local fortune = handle:read("*a")
+        handle:close()
+        dashboard.section.footer.val = fortune
+
+        dashboard.config.opts.noautocmd = true
+
+        dashboard.config.layout = {
+            { type = "padding", val = 10 },
+            dashboard.section.header,
+            { type = "padding", val = 2 },
+            dashboard.section.buttons,
+            { type = "padding", val = 1 },
+            dashboard.section.footer,
+        }
+
+        alpha.setup(dashboard.config)
     end
 };
