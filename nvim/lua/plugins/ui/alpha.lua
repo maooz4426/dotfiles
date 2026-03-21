7a3b7ba4355a08cfeb712e542b1259de1c8c1d0b
MAOZ <maooz4426@gmail.com>
2026-03-22 01:12:15 +0900

    ディレクトリ分け

--- nvim/lua/plugins/ui/alpha.lua
+++ nvim/lua/plugins/ui/alpha.lua
@@ -19,13 +19,13 @@ return {
             [[`--'  `--'  `------'     `-----'      `-'      `--'   `--'   `--' ]],
         }
         dashboard.section.buttons.val = {
-            dashboard.button('f', '' .. ' Find file', ':Telescope find_files <CR>'),
-            dashboard.button('e', '' .. ' New file', ':ene <BAR> startinsert <CR>'),
-            dashboard.button('r', '' .. ' Recent files', ':Telescope oldfiles <CR>'),
-            dashboard.button('t', '' .. ' Find text', ':Telescope live_grep <CR>'),
-            dashboard.button('c', '' .. ' Config', ':e ~/.config/nvim/init.lua <CR>'),
+            dashboard.button('f', '' .. ' Find file', ':Telescope find_files <CR>'),
+            dashboard.button('e', '' .. ' New file', ':ene <BAR> startinsert <CR>'),
+            dashboard.button('r', '' .. ' Recent files', ':Telescope oldfiles <CR>'),
+            dashboard.button('t', '' .. ' Find text', ':Telescope live_grep <CR>'),
+            dashboard.button('c', '' .. ' Config', ':e ~/.config/nvim/init.lua <CR>'),
             dashboard.button('n', '󰎜' .. ' New Note', ':Neorg workspace notes <CR>'),
-            dashboard.button('q', '' .. ' Quit', ':qa<CR>'),
+            dashboard.button('q', '' .. ' Quit', ':qa<CR>'),
         }
         local handle = io.popen('fortune')
         local fortune = handle:read("*a")
--- nvim/lua/plugins/ui/nvim-tree.lua
+++ nvim/lua/plugins/ui/nvim-tree.lua
@@ -15,5 +15,4 @@ return {
         })
         vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
     end,
-
 }
