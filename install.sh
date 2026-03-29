925aaaa4698e6c838523ddd7716d297f8e8f6363
MAOZ <maooz4426@gmail.com>
2026-03-30 00:35:58 +0900

    add install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ brew install Neovim
 
 # CLI tools
 brew install tmux
+brew install tpm
 
 # Window border decoration
 brew install FelixKratz/formulae/borders
@@ -15,3 +16,5 @@ brew install ripgrep fd
 # Symlink configs
 ln -sf "$PWD/nvim" "$HOME/.config/nvim"
 ln -sf "$PWD/borders/bordersrc" "$HOME/.config/borders/bordersrc"
+ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
