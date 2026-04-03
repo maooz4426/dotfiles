1949f8b05258a7e86de588de9dbcdd2a38ff3d77
MAOZ <maooz4426@gmail.com>
2026-04-04 00:28:00 +0900

    create module for karabiner

---
+++ nix/home/modules/karabiner.nix
@@ -0,0 +1,35 @@
+{...}: {
+    home.file.karabinerMouseVim = {
+        target = ".config/karabiner/assets/complex_modifications/mouse_vim.json";
+        text = builtins.toJSON {
+            title = "Mouse Vim Keys (Ctrl + Opt + hjkl)";
+            rules = [
+                {
+                    description = "Ctrl + Opt + hjkl to mouse movement";
+                    manipulators = [
+                        {
+                            type = "basic";
+                            from = { key_code = "h"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
+                            to = [{ mouse_key = { x = -1536; }; }];
+                        }
+                        {
+                            type = "basic";
+                            from = { key_code = "j"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
+                            to = [{ mouse_key = { y = 1536; }; }];
+                        }
+                        {
+                            type = "basic";
+                            from = { key_code = "k"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
+                            to = [{ mouse_key = { y = -1536; }; }];
+                        }
+                        {
+                            type = "basic";
+                            from = { key_code = "l"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
+                            to = [{ mouse_key = { x = 1536; }; }];
+                        }
+                    ];
+                }
+            ];
+        };
+    };
+}
--- nix/home/profiles/maozbook/default.nix
+++ nix/home/profiles/maozbook/default.nix
@@ -1,6 +1,7 @@
 {pkgs,...}: {
     imports = [
         ../../modules/tmux.nix
+        ../../modules/karabiner.nix
     ];
 
     home.stateVersion = "25.05";
@@ -19,37 +20,4 @@
         enable = true;
     };
 
-    home.file.karabinerMouseVim = {
-        target = ".config/karabiner/assets/complex_modifications/mouse_vim.json";
-        text = builtins.toJSON {
-            title = "Mouse Vim Keys (Ctrl + Opt + hjkl)";
-            rules = [
-                {
-                    description = "Ctrl + Opt + hjkl to mouse movement";
-                    manipulators = [
-                        {
-                            type = "basic";
-                            from = { key_code = "h"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
-                            to = [{ mouse_key = { x = -1536; }; }];
-                        }
-                        {
-                            type = "basic";
-                            from = { key_code = "j"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
-                            to = [{ mouse_key = { y = 1536; }; }];
-                        }
-                        {
-                            type = "basic";
-                            from = { key_code = "k"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
-                            to = [{ mouse_key = { y = -1536; }; }];
-                        }
-                        {
-                            type = "basic";
-                            from = { key_code = "l"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
-                            to = [{ mouse_key = { x = 1536; }; }];
-                        }
-                    ];
-                }
-            ];
-        };
-    };
 }
