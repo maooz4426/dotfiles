f98aa0d8242488181aeba8ad214516ad1873008d
MAOZ <maooz4426@gmail.com>
2026-04-04 00:11:21 +0900

    change control mouse

--- nix/home/profiles/maozbook/default.nix
+++ nix/home/profiles/maozbook/default.nix
@@ -1,4 +1,8 @@
 {pkgs,...}: {
+    imports = [
+        ../../modules/tmux.nix
+    ];
+
     home.stateVersion = "25.05";
     home.username = "maoz";
     home.homeDirectory = "/Users/maoz";
@@ -18,29 +22,29 @@
     home.file.karabinerMouseVim = {
         target = ".config/karabiner/assets/complex_modifications/mouse_vim.json";
         text = builtins.toJSON {
-            title = "Mouse Vim Keys (Ctrl + hjkl)";
+            title = "Mouse Vim Keys (Ctrl + Opt + hjkl)";
             rules = [
                 {
-                    description = "Ctrl + hjkl to mouse movement";
+                    description = "Ctrl + Opt + hjkl to mouse movement";
                     manipulators = [
                         {
                             type = "basic";
-                            from = { key_code = "h"; modifiers = { mandatory = ["left_control"]; optional = ["any"]; }; };
+                            from = { key_code = "h"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
                             to = [{ mouse_key = { x = -1536; }; }];
                         }
                         {
                             type = "basic";
-                            from = { key_code = "j"; modifiers = { mandatory = ["left_control"]; optional = ["any"]; }; };
+                            from = { key_code = "j"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
                             to = [{ mouse_key = { y = 1536; }; }];
                         }
                         {
                             type = "basic";
-                            from = { key_code = "k"; modifiers = { mandatory = ["left_control"]; optional = ["any"]; }; };
+                            from = { key_code = "k"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
                             to = [{ mouse_key = { y = -1536; }; }];
                         }
                         {
                             type = "basic";
-                            from = { key_code = "l"; modifiers = { mandatory = ["left_control"]; optional = ["any"]; }; };
+                            from = { key_code = "l"; modifiers = { mandatory = ["left_control" "left_option"]; optional = ["any"]; }; };
                             to = [{ mouse_key = { x = 1536; }; }];
                         }
                     ];
