993216ea330e7052622fc053df42746865610944
MAOZ <maooz4426@gmail.com>
2026-04-09 01:17:28 +0900

    neovimをmodule化

--- nix/home/profiles/maozbook/default.nix
+++ nix/home/profiles/maozbook/default.nix
@@ -2,7 +2,7 @@
     imports = [
         ../../modules/tmux.nix
         ../../modules/karabiner.nix
-        nixCats.homeModule
+        ../../modules/neovim.nix
     ];
 
     home.stateVersion = "25.05";
@@ -26,69 +26,6 @@
 
     programs.gh = {
         enable = true;
-    };
-
-    nixCats = {
-        enable = true;
-        packageNames = [ "myNvim" ];
-
-        luaPath = ../../../../nvim;
-
-        categoryDefinitions.replace = ({ pkgs, ... }: {
-            startupPlugins = {
-                lsp = with pkgs.vimPlugins; [
-                    nvim-lspconfig
-                    nvim-cmp
-                    cmp-nvim-lsp
-                    cmp-buffer
-                    cmp-path
-                    luasnip
-                    cmp_luasnip
-                ];
-                ui = with pkgs.vimPlugins; [
-                    alpha-nvim
-                    mini-nvim
-                    plenary-nvim
-                    kanagawa-nvim
-                    nvim-tree-lua
-                    nvim-web-devicons
-                ];
-                editor = with pkgs.vimPlugins; [
-                    (nvim-treesitter.withPlugins (p: [
-                        p.go p.bash p.c p.diff p.html p.javascript p.json
-                        p.lua p.luadoc p.luap p.markdown p.python p.toml
-                        p.tsx p.typescript p.yaml
-                    ]))
-                    telescope-nvim
-                    toggleterm-nvim
-                    markdown-preview-nvim
-                ];
-            };
-        });
-
-        packageDefinitions = {
-            replace = {
-                myNvim = { pkgs, ... }: {
-                    settings = {
-                        wrapRc = false;
-                        aliases = [ "nvim" "vim" "vi" ];
-                    };
-                    categories = {
-                        lsp = true;
-                        ui = true;
-                        editor = true;
-                    };
-                };
-            };
-        };
-    };
-
-    xdg.configFile."nvim/init.lua" = lib.mkForce {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Develop/dotfiles/nvim/init.lua";
-    };
-
-    xdg.configFile."nvim/lua" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Develop/dotfiles/nvim/lua";
     };
 
 }
