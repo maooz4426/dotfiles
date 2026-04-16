2ab9db5d414effed4ff57bf3f28ac45d40235ff0
MAOZ <maooz4426@gmail.com>
2026-04-17 00:26:00 +0900

    add gitleaks

--- nix/home/profiles/maozbook/default.nix
+++ nix/home/profiles/maozbook/default.nix
@@ -1,38 +1,46 @@
 # home-manager設定のエントリーポイント。
 # 各アプリのモジュールをimportし、共通パッケージをまとめて宣言する。
-{pkgs, config, lib, nixCats, ...}: {
-    imports = [
-        ../../modules/tmux.nix
-        ../../modules/karabiner.nix
-        ../../modules/neovim.nix
-        ../../modules/starship.nix
-        ../../modules/zsh.nix
-        ../../modules/ghostty.nix
-    ];
+{
+  pkgs,
+  config,
+  lib,
+  nixCats,
+  ...
+}:
+{
+  imports = [
+    ../../modules/tmux.nix
+    ../../modules/karabiner.nix
+    ../../modules/neovim.nix
+    ../../modules/starship.nix
+    ../../modules/zsh.nix
+    ../../modules/ghostty.nix
+  ];
 
-    home.stateVersion = "25.05";
-    home.username = "maoz";
-    home.homeDirectory = "/Users/maoz";
+  home.stateVersion = "25.05";
+  home.username = "maoz";
+  home.homeDirectory = "/Users/maoz";
 
-    home.packages = [
-        pkgs.yazi                      # ターミナルファイルマネージャー
-        pkgs.mosh                      # モバイル向けSSHクライアント
-        pkgs.gcc                       # Cコンパイラ
-        pkgs.clang-tools               # clangd等のC/C++ツール
-        pkgs.lua-language-server       # Lua LSP
-        pkgs.typescript-language-server # TypeScript LSP
-        pkgs.gopls                     # Go LSP
-        pkgs.nil                       # Nix LSP
-        pkgs.starship                  # シェルプロンプト
-        pkgs.mariadb.client            # MySQLクライアント（CLIのみ）
-        pkgs.terraform-ls
-    ];
+  home.packages = [
+    pkgs.yazi # ターミナルファイルマネージャー
+    pkgs.mosh # モバイル向けSSHクライアント
+    pkgs.gcc # Cコンパイラ
+    pkgs.clang-tools # clangd等のC/C++ツール
+    pkgs.lua-language-server # Lua LSP
+    pkgs.typescript-language-server # TypeScript LSP
+    pkgs.gopls # Go LSP
+    pkgs.nil # Nix LSP
+    pkgs.starship # シェルプロンプト
+    pkgs.mariadb.client # MySQLクライアント（CLIのみ）
+    pkgs.terraform-ls
+    pkgs.gitleaks
+  ];
 
-    programs.go = {
-        enable = true;
-    };
+  programs.go = {
+    enable = true;
+  };
 
-    programs.gh = {
-        enable = true; # GitHub CLI
-    };
+  programs.gh = {
+    enable = true; # GitHub CLI
+  };
 }
