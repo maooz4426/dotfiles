49bc35cdfca2f0162d36dfdd8480be59aa0d18e1
MAOZ <maooz4426@gmail.com>
2026-04-26 01:45:36 +0900

    set keymap ctrl-q is changing normal mode

--- nvim/lua/core/key.lua
+++ nvim/lua/core/key.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit" })
-vim.keymap.set("t", "jj", "<c-\\><c-n>", { desc = "Change to Normal Mode" })
+vim.keymap.set("t", "<C-q>", "<c-\\><c-n>", { desc = "Change to Normal Mode" })
