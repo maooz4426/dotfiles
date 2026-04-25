daa8ff10ee0bef957d539096b7373ccbbadec405
MAOZ <maooz4426@gmail.com>
2026-04-26 02:02:58 +0900

    add go mod tidy to conform.nvim

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -6,11 +6,19 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
+        go = { "goimports" },
       },
       format_on_save = {
         timeout_ms = 500,
         lsp_format = "fallback",
       },
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = "*.go",
+      callback = function()
+        vim.fn.jobstart({ "go", "mod", "tidy" }, { cwd = vim.fn.getcwd() })
+      end,
     })
   end,
 }
